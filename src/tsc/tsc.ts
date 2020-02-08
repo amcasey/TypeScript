f6a30c917635cfb807a38beb047a68282d2b732c
Andrew Casey <andrew.casey@microsoft.com>
2020-02-07 17:42:49 -0800

    More notes

--- src/tsc/tsc.ts
+++ src/tsc/tsc.ts
@@ -14,5 +14,11 @@ if (ts.sys.setBlocking) {
 
 ts.executeCommandLine(ts.sys, ts.noop, ts.sys.args); // TODO (acasey): I don't see how we can honor callbacks in parallel mode - I don't believe they can cross thread boundaries
 
-// TODO (acasey): assuming this is the entry point of the helper threads, there needs to be a way to build a single project
-// Manipulating args is probably sufficient, but it will likely require a bunch of redundant up-to-date checks
+// TODO (acasey): need a way to detect we're a helper thread/process
+//  Build only one project
+//  Don't do up-to-date checks
+//  Don't watch files/dirs
+// Trivial for threads - several imports indicate whether this is the main thread or a child
+// Processes can tell whether they have an IPC channel to a parent process, but there are probably already callers forking us from node processes
+
+// TODO (acasey): What's the entrypoint for the web-hosted version of the compiler?
